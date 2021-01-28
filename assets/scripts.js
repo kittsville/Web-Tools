1059e628994fa2672849428f392b52e983af8d56
kittsville <kit.maywood@ovoenergy.com>
2021-01-28 13:35:25 +0000

    Add stack trace extractor

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,3 +1,12 @@
+document.getElementById('log-text').addEventListener('input', e => {
+  const logText = e.target.value;
+  const rawJson = logText.split(/({.+)/)[1];
+  const json = JSON.parse(rawJson);
+  const stackTrace = json.hasOwnProperty('StackTrace') ? json['StackTrace'] : json['stack_trace'];
+
+  document.getElementById('stack-trace').textContent = stackTrace;
+});
+
 // Initialises Material Design Components
 // See: https://github.com/material-components/material-components-web#javascript
 Array.from(document.getElementsByClassName('mdc-text-field')).forEach(mdc.textField.MDCTextField.attachTo);
