343071b98b958dc08385a6163133310decadd509
Kit <kittsville@googlemail.com>
2021-01-30 17:05:14 +0000

    Add example stack extractor log

--- stack-extractor/scripts.js
+++ stack-extractor/scripts.js
@@ -1,4 +1,6 @@
-document.getElementById('log-text').addEventListener('input', e => {
+const textarea = document.getElementById('log-text');
+
+textarea.addEventListener('input', e => {
   let stackTrace;
 
   try {
@@ -18,4 +20,14 @@ document.getElementById('log-text').addEventListener('input', e => {
   }
 
   document.getElementById('stack-trace').textContent = stackTrace;
+});
+
+const exampleLog = `Example text | thread-4 | {"timestamp":"2019-11-19 15:42:35.891","level":"ERROR","mdc":{"appVersion":null,"appType":null,"User-Agent":"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:70.0) Gecko/20100101 Firefox/70.0","ipAddress":"127.0.0.1","member":"1","BuildVersion":null,"queryString":"id=736145632145","uri":"/register/needDetail","HostName":"urbanpro-Vostro-3583"},"logger":"org.grails.web.errors.GrailsExceptionResolver","stack_trace":"Exception in thread \\"main\\"\\njava.lang.NullPointerException\\n        at com.example.myproject.Book.getTitle(Book.java:16)\\n        at com.example.myproject.Author.getBookTitles(Author.java:25)\\n        at com.example.myproject.Bootstrap.main(Bootstrap.java:14)"}`
+
+document.getElementById('see-example').addEventListener('click', () => {
+  textarea.value = exampleLog;
+  textarea.dispatchEvent(new Event('input', {
+    bubbles: true,
+    cancelable: true,
+  }));
 });
