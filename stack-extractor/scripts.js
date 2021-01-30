4d09d85672f53c9deb64df22b618150221e3441c
Kit <kittsville@googlemail.com>
2021-01-30 16:26:45 +0000

    Display error if stack extraction fails

--- stack-extractor/scripts.js
+++ stack-extractor/scripts.js
@@ -1,8 +1,14 @@
 document.getElementById('log-text').addEventListener('input', e => {
-  const logText = e.target.value;
-  const rawJson = logText.split(/({.+)/)[1];
-  const json = JSON.parse(rawJson);
-  const stackTrace = json.hasOwnProperty('StackTrace') ? json['StackTrace'] : json['stack_trace'];
+  let stackTrace;
+
+  try {
+    const logText = e.target.value;
+    const rawJson = logText.split(/({.+)/)[1];
+    const json = JSON.parse(rawJson);
+    stackTrace = json.hasOwnProperty('StackTrace') ? json['StackTrace'] : json['stack_trace'];
+  } catch (error) {
+    stackTrace = "Failed to parse log:\n" + error.message;
+  }
 
   document.getElementById('stack-trace').textContent = stackTrace;
 });
