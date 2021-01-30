240e78aa6dff416343965b72a813f6740acd9b8c
Kit <kittsville@googlemail.com>
2021-01-30 17:03:57 +0000

    Error if JSON missing stack trace

--- stack-extractor/scripts.js
+++ stack-extractor/scripts.js
@@ -5,7 +5,14 @@ document.getElementById('log-text').addEventListener('input', e => {
     const logText = e.target.value;
     const rawJson = logText.split(/({.+)/)[1];
     const json = JSON.parse(rawJson);
-    stackTrace = json.hasOwnProperty('StackTrace') ? json['StackTrace'] : json['stack_trace'];
+
+    if (json.hasOwnProperty('StackTrace')) {
+      stackTrace = json['StackTrace'];
+    } else if (json.hasOwnProperty('stack_trace')) {
+      stackTrace = json['stack_trace'];
+    } else {
+      throw new Error("JSON parsed but no 'StackTrace' or 'stack_trace' property found");
+    }
   } catch (error) {
     stackTrace = "Failed to parse log:\n" + error.message;
   }
