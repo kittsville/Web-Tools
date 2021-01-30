5aa416d8501cfc43338027ff7a79f6c27ebe9ded
Kit <kittsville@googlemail.com>
2021-01-30 15:52:34 +0000

    Stylise tool list

--- shared/scripts.js
+++ shared/scripts.js
@@ -2,3 +2,4 @@
 // See: https://github.com/material-components/material-components-web#javascript
 Array.from(document.getElementsByClassName('mdc-text-field')).forEach(mdc.textField.MDCTextField.attachTo);
 Array.from(document.getElementsByTagName('button')).forEach(mdc.iconButton.MDCIconButtonToggle.attachTo);
+Array.from(document.getElementsByTagName('ul')).forEach(mdc.list.MDCList.attachTo);
