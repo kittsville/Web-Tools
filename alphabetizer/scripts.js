e37f4f6ba83ed8e760eff462fb931d4d973b53a4
Kit <kit@tray.io>
2022-05-04 11:48:46 +0100

    Add rev sort

--- alphabetizer/scripts.js
+++ alphabetizer/scripts.js
@@ -2,10 +2,12 @@ const textarea = document.getElementById('input-text');
 
 const matchBlocks = /([A-Z][a-z]+)/g
 
-const trim = (input) => input.split('\n').sort().join('\n');
+const sort = (input) => input.split('\n').sort().join('\n');
+const revSort = (input) => input.split('\n').sort((a, b) => a < b).join('\n');
 
 textarea.addEventListener('input', e => {
-  document.getElementById('output-text').textContent = trim(e.target.value);
+  document.getElementById('output-text').textContent = sort(e.target.value);
+  document.getElementById('output-text-2').textContent = revSort(e.target.value);
 });
 
 const exampleInput = `Bacon
