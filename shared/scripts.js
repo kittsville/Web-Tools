c38ef5607e5cec2263083da56e5fa78cce502d97
Kit <kit@tray.io>
2022-05-09 11:16:07 +0100

    Add copying result to clipboard

--- shared/scripts.js
+++ shared/scripts.js
@@ -3,3 +3,18 @@
 Array.from(document.getElementsByClassName('mdc-text-field')).forEach(mdc.textField.MDCTextField.attachTo);
 Array.from(document.getElementsByTagName('button')).forEach(mdc.iconButton.MDCIconButtonToggle.attachTo);
 Array.from(document.getElementsByTagName('ul')).forEach(mdc.list.MDCList.attachTo);
+
+Array.from(document.getElementsByTagName('pre')).forEach(preEl => {
+    const copyEl = document.createElement('span');
+    copyEl.classList.add('material-icons', 'copy');
+    copyEl.textContent = 'content_copy';
+    copyEl.title = 'Copy to clipboard';
+    copyEl.addEventListener('click', () => navigator.clipboard.writeText(preEl.textContent));
+
+
+    const wrapperEl = document.createElement('div');
+    wrapperEl.classList.add('pre-wrapper');
+    wrapperEl.appendChild(preEl);
+    wrapperEl.appendChild(copyEl);
+    document.querySelector('main').appendChild(wrapperEl);
+});
